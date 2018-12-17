dcba6f1be7b32e7d3c76d1218f7b8210a47c2ec5
Shashwat Koliwad <koliwad96@gmail.com>
2018-12-17 19:28:40 +0530

    stuff
    base layout, login and logout
    
    closes #2

--- dj_bookmarks/dj_bookmarks/urls.py
+++ dj_bookmarks/dj_bookmarks/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'login/$', auth_views.LoginView.as_view(template_name = 'auth/login.html'), name= 'login'),
+    url(r'logout/$', auth_views.LogoutView.as_view(), name='logout')
+
 ]
